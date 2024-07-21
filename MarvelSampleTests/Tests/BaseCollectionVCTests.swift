46362cbd145d19e6a4732142a282c879f5e671ec
Hiren Fadadu <hirenfaldu9697@gmail.com>
2024-07-21 18:00:19 +0530

    Added more test for BaseCollectionVC

--- MarvelSampleTests/Tests/BaseCollectionVCTests.swift
+++ MarvelSampleTests/Tests/BaseCollectionVCTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import MarvelSample
 
 final class BaseCollectionVCTests: XCTestCase {
-
+    
     var sut: TestableChildCollectionVC!
     var viewModel: TestableAPIDataListable!
     
@@ -30,7 +30,7 @@ final class BaseCollectionVCTests: XCTestCase {
 
 // MARK: - UI Components
 extension BaseCollectionVCTests {
-   
+    
     func test_correctViewModelObject() {
         XCTAssertTrue(sut.viewModel === viewModel, "viewModel object is different")
     }
@@ -56,16 +56,15 @@ extension BaseCollectionVCTests {
     func test_refreshController_hasTarget() {
         var number = 0
         sut.refreshHandler = {
-           number += 1
+            number += 1
         }
         triggerRefresh(sut.refreshControl)
         XCTAssertEqual(number, 1)
     }
 }
 
-// MARK: - Bindgs
+// MARK: - CollectionView Sections
 extension BaseCollectionVCTests {
-    
     func test_fetchState_initialLoading_shouldShowZeroCollectionViewSections() {
         viewModel.fetchState.value = DataFetchState.initialLoading
         let numberOfSections = numberOfSections(in: sut.collectionView)
@@ -81,27 +80,76 @@ extension BaseCollectionVCTests {
         viewModel.fetchState.value = DataFetchState.emptyData
         XCTAssertEqual(numberOfSections(in: sut.collectionView), 1)
     }
-     
+    
     func test_fetchState_error_shouldShowOneCollectionViewSections() {
         viewModel.fetchState.value = DataFetchState.error(NetworkError.invalidURL)
         XCTAssertEqual(numberOfSections(in: sut.collectionView), 1)
-    } 
+    }
     
     func test_fetchState_loadingNextPage_shouldShowOneCollectionViewSections() {
         viewModel.fetchState.value = DataFetchState.loadingNextPage
         XCTAssertEqual(numberOfSections(in: sut.collectionView), 1)
-    } 
+    }
     
     func test_fetchState_reload_shouldShowOneCollectionViewSections() {
         viewModel.fetchState.value = DataFetchState.reload
         XCTAssertEqual(numberOfSections(in: sut.collectionView), 1)
     }
-    
+}
+
+// MARK: - Loader
+extension BaseCollectionVCTests {
     func test_fetchState_initialLoading_shouldShowLoader() {
         viewModel.fetchState.value = DataFetchState.initialLoading
-        XCTAssertTrue(!sut.loaderContainer.isHidden, "Loader is not visible")
+        XCTAssertFalse(sut.loaderContainer.isHidden, "Loader is not visible")
+        XCTAssertTrue(sut.loader.isAnimating, "Loader is not animating")
     }
     
+    func test_fetchState_reloading_shouldNotShowLoader() {
+        viewModel.fetchState.value = .reload
+        XCTAssertTrue(sut.loaderContainer.isHidden, "Loader is visible")
+        XCTAssertFalse(sut.loader.isAnimating, "Loader is animating")
+    }
+    
+    func test_fetchState_idle_shouldNotShowLoader() {
+        viewModel.fetchState.value = .idle
+        XCTAssertTrue(sut.loaderContainer.isHidden, "Loader is visible")
+        XCTAssertFalse(sut.loader.isAnimating, "Loader is animating")
+    }
+    
+    func test_fetchState_emptyData_shouldNotShowLoader() {
+        viewModel.fetchState.value = .emptyData
+        XCTAssertTrue(sut.loaderContainer.isHidden, "Loader is visible")
+        XCTAssertFalse(sut.loader.isAnimating, "Loader is animating")
+    }
+    
+    func test_fetchState_error_shouldNotShowLoader() {
+        viewModel.fetchState.value = .error(NetworkError.invalidURL)
+        XCTAssertTrue(sut.loaderContainer.isHidden, "Loader is visible")
+        XCTAssertFalse(sut.loader.isAnimating, "Loader is animating")
+    }
+    
+    func test_fetchState_loadingNextPage_shouldNotShowLoader() {
+        viewModel.fetchState.value = .loadingNextPage
+        XCTAssertTrue(sut.loaderContainer.isHidden, "Loader is visible")
+        XCTAssertFalse(sut.loader.isAnimating, "Loader is animating")
+    }
+    
+    func test_fetchState_updateFromIdleToInitialLoadingAndAgainInIdel_shouldShowHideLoaderAccordingly() {
+        viewModel.fetchState.value = .idle
+        XCTAssertTrue(sut.loaderContainer.isHidden, "Loader is not hidden in idle state")
+        XCTAssertFalse(sut.loader.isAnimating, "Loader is animating in idle state")
+        viewModel.fetchState.value = .initialLoading
+        XCTAssertFalse(sut.loaderContainer.isHidden, "Loader is not shown in initialLoading state")
+        XCTAssertTrue(sut.loader.isAnimating, "Loader is not animating in initialLoading state")
+        viewModel.fetchState.value = .idle
+        XCTAssertTrue(sut.loaderContainer.isHidden, "Loader is not hidden idle state")
+        XCTAssertFalse(sut.loader.isAnimating, "Loader is animating in idle state")
+    }
+}
+
+// MARK: - Number and type of cell
+extension BaseCollectionVCTests {
     func test_fetchState_initialLoading_shouldShowZeroItemsInSection() {
         viewModel.fetchState.value = DataFetchState.initialLoading
         XCTAssertEqual(numberOfRows(in: sut.collectionView), 0)
@@ -142,7 +190,10 @@ extension BaseCollectionVCTests {
         XCTAssertTrue(cell is ErrorCC,
                       "cell is not ErrorCC")
     }
-    
+}
+
+// MARK: - Next page loader
+extension BaseCollectionVCTests {
     func test_fetchState_initialLoading_shouldNotShowNextPageLoader() {
         viewModel.fetchState.value = .initialLoading
         let footerReferenceSize = referenceSizeForFooterView(in: sut.collectionView)
@@ -151,6 +202,24 @@ extension BaseCollectionVCTests {
     
     func test_fetchState_idle_shouldNotShowNextPageLoader() {
         viewModel.fetchState.value = .idle
+        let footerReferenceSize = referenceSizeForFooterView(in: sut.collectionView)
+        XCTAssertEqual(footerReferenceSize, .zero)
+    }
+    
+    func test_fetchState_emptyData_shouldNotShowNextPageLoader() {
+        viewModel.fetchState.value = .emptyData
+        let footerReferenceSize = referenceSizeForFooterView(in: sut.collectionView)
+        XCTAssertEqual(footerReferenceSize, .zero)
+    }
+    
+    func test_fetchState_error_shouldNotShowNextPageLoader() {
+        viewModel.fetchState.value = .error(NetworkError.invalidURL)
+        let footerReferenceSize = referenceSizeForFooterView(in: sut.collectionView)
+        XCTAssertEqual(footerReferenceSize, .zero)
+    }
+    
+    func test_fetchState_reloading_shouldNotShowNextPageLoader() {
+        viewModel.fetchState.value = .reload
         let footerReferenceSize = referenceSizeForFooterView(in: sut.collectionView)
         XCTAssertEqual(footerReferenceSize, .zero)
     }
