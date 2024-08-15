6ee75e1ad89f5381a72c28bf625fe7bb48defe74
Hiren Fadadu <hirenfaldu9697@gmail.com>
2024-08-15 09:10:12 +0530

    Added documentation in TabBarController and TabBarTests

--- MarvelSampleTests/Tests/TabBarTests.swift
+++ MarvelSampleTests/Tests/TabBarTests.swift
@@ -8,6 +8,10 @@
 import XCTest
 @testable import MarvelSample
 
+/// Tests:
+/// 1. Number of viewControllers
+/// 2. Sequence of viewControllers
+/// 3. Tab information
 final class TabBarTests: XCTestCase {
 
     var viewModel: TabBarVM!
@@ -55,9 +59,8 @@ extension TabBarTests {
     }
 }
 
-// MARK: - Tests with Three ViewControllers
+// MARK: - Number of viewControllers
 extension TabBarTests {
-    
     func test_numberOfViewControllers() {
         guard let viewControllers = sut.viewControllers else {
             XCTFail("Precondition: Found viewControllers nil")
@@ -65,7 +68,10 @@ extension TabBarTests {
         }
         XCTAssertEqual(sut.viewControllers!.count, viewModel.tabBarItemVMs.count)
     }
-    
+}
+
+// MARK: - Sequence of viewControllers
+extension TabBarTests {
     func test_viewControllerSequence() {
         guard let viewControllers = sut.viewControllers else {
             XCTFail("Precondition: Found viewControllers nil")
@@ -73,7 +79,10 @@ extension TabBarTests {
         }
         XCTAssertEqual(viewControllers, sut.arrViewController)
     }
-    
+}
+
+// MARK: - Tab information
+extension TabBarTests {
     func test_firstViewController_tabInformation() {
         guard let viewControllers = sut.viewControllers else {
             XCTFail("Precondition: Found viewControllers nil")
