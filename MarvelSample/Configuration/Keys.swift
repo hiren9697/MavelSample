6521fb5baf91d729417f47dcd86a67d782cdef2b
Hiren Fadadu <hirenfaldu9697@gmail.com>
2024-12-11 06:50:56 +0530

    Tracked 'Keys.swift' file

---
+++ MarvelSample/Configuration/Keys.swift
@@ -0,0 +1,17 @@
+//
+//  Keys.swift
+//  MarvelSample
+//
+//  Created by Hirenkumar Fadadu on 06/07/24.
+//
+
+import Foundation
+
+struct Keys {
+    static var publicKey: String {
+        fatalError("Enter your own public key")
+    }
+    static var privateKey: String {
+        fatalError("Enter your own private key")
+    }
+}
